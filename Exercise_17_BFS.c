5c3680b83876a9a3dfd7b688121fb557f1ea2d4c
AnjanaPT <74732658+AnjanaPT@users.noreply.github.com>
2021-03-08 19:48:25 +0530

    Add files via upload

---
+++ Exercise_17_BFS.c
@@ -0,0 +1,108 @@
+#include<stdio.h>
+int vertex[50],visited[50],bfs[50],graph[50][50];
+int nv,f,r;
+int search(int a[],int b,int s);
+void printVertex(int s);
+void bfsT(int);
+void main()
+{
+    int i,j,v,ad,src;
+    printf("\nEnter The Number Of Vertices : ");
+    scanf("%d",&nv);
+    printf("\n-------------------------------------------\nEnter The Vertices\n-------------------------------------------");
+    for(i=0;i<nv;i++)
+    {
+        vertex:
+        printf("\nEnter Vertex %d  : ",i+1);
+        scanf("%d",&v);
+        if(search(vertex,v,i)==1)
+        {
+            printVertex(i);
+            printf("\nVertex %d Already Exists !! \n-------------------------------------------",v);goto vertex;
+        }
+        vertex[i]=v;//vertex
+        visited[v]=0;//to indicate visited or not
+    }
+    printf("\n-------------------------------------------\nEnter Graph In Matrix Form( %d by %d Matrix, 1 : Presence Of Edge, 0 : Absence Of Edge )\n",nv,nv);
+    for(i=0;i<nv;i++)
+    {
+        for(j=0;j<nv;j++)
+        {
+            adjacent:
+            scanf("%d",&ad);
+            if(ad!=0 && ad!=1)
+            {
+                printf("\nYou Can Only Enter 0 or 1 !!\n");goto adjacent;
+            }
+            graph[vertex[i]][vertex[j]]=ad;
+        }
+        printf("\n");
+    }
+    printf("     ");
+    for(j=0;j<nv;j++)
+    {
+        printf("%d  ",vertex[j]);
+    }
+    printf("\n    ");
+    for(j=0;j<nv;j++)printf("---");
+    for(i=0;i<nv;i++)
+    {
+        printf("\n%d  | ",vertex[i]);
+        for(j=0;j<nv;j++)
+        {
+            printf("%d  ",graph[vertex[i]][vertex[j]]);
+        }
+    }
+    source:
+    printf("\n-------------------------------------------\nEnter The Source Vertex : ");
+    scanf("%d",&src);
+    if(search(vertex,src,nv)==0)
+    {
+        printVertex(nv);
+        printf("\nVertex %d Not Found !! \n-------------------------------------------",src);goto source;
+    }
+    printVertex(nv);
+    f=0;r=0;
+    bfs[r]=bfs[f]=src;
+    visited[src]=1;
+    printf("\nBFS TRAVERSAL	  : %d",src);   
+    bfsT(src);
+    printf("\nRemaining Vertices Are Unreachable,if any !! \n-------------------------------------------");
+}
+void bfsT(int v)
+{
+    int i;
+    for(i=0;i<nv;i++)
+     {
+       if(graph[v][vertex[i]]==1 && visited[vertex[i]]==0)
+        {
+          printf("  %d",vertex[i]);
+	  visited[vertex[i]]=1;
+	  r++;
+          bfs[r]=vertex[i];
+        }
+     }
+    while(f<=r)
+    {
+     bfsT(bfs[f++]);
+    }
+    return;
+}
+int search(int a[],int b,int s)
+{
+    int i;
+    for(i=0;i<s;i++)
+    {
+        if(b==a[i])return 1;
+    }
+    return 0;
+}
+void printVertex(int s)
+{
+    int i;
+    printf("\n-------------------------------------------\nExisting Vertices : ");
+    for(i=0;i<s;i++)
+    {
+        printf("%d  ",vertex[i]);
+    }
+}
